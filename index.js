d5dbe0631201cd09f288f91d0d1d0a3f86819fe1
Maryna <maryna@thinkingbox.com>
2019-11-04 17:39:34 -0500

    displayeed selected dog img

--- index.js
+++ index.js
@@ -19,37 +19,39 @@
 
 // 
 
-window.onload = function () {
-  updateBitcoin()
-}
+// window.onload = function () {
+//   updateBitcoin()
+// }
 
-function bitcoin() {
-  const promise = fetch("https://blockchain.info/q/24hrprice?cors=true");
+// function bitcoin() {
+//   const promise = fetch("https://blockchain.info/q/24hrprice?cors=true");
 
-  promise.then(function (response) {
-      const processingPromise = response.json();
-      return processingPromise;
+//   promise.then(function (response) {
+//       const processingPromise = response.json();
+//       return processingPromise;
 
-    })
-    .then(function (processingPromise) {
-      let currentValue = `${processingPromise} USD`;
-      return updateBitcoin(currentValue);
-    })
-}
+//     })
+//     .then(function (processingPromise) {
+//       let currentValue = `${processingPromise} USD`;
+//       return updateBitcoin(currentValue);
+//     })
+// }
 
 
-function updateBitcoin(value) {
+// function updateBitcoin(value) {
 
-  const span = document.getElementById('bitcoin-value')
-  span.innerHTML = value;
+//   const span = document.getElementById('bitcoin-value')
+//   span.innerHTML = value;
 
-  var time = 1000;
+//   var time = 1000;
 
-  setTimeout(bitcoin, time)
-}
+//   setTimeout(bitcoin, time)
+// }
 
 //Breeds
 const breeds = document.querySelector("#breeds");
+var btnLoad = document.querySelector("#btn-load");
+const doggos = document.querySelector(".doggos");
 
 
 var request = new XMLHttpRequest()
@@ -64,10 +66,41 @@ request.onload = function () {
 
   result.forEach(breed => {
     option = document.createElement("option");
+    option.value = breed;
     option.innerHTML = breed;
     breeds.appendChild(option);
   })
-
 }
 
 request.send();
+
+btnLoad.onclick = function (e) {
+  var breed = document.querySelector("#breeds").value;
+  loadBreedImages(breed);
+};
+
+function loadBreedImages(breed) {
+  // See https://dog.ceo/dog-api/documentation/breed
+  // Use the imageCount and breed variables to create our URL 
+  var imageCount = document.querySelector("#imageCount").value;
+  var url = `https://dog.ceo/api/breed/${breed}/images/random/${imageCount}`;
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function () {
+    var response = JSON.parse(this.responseText);
+    result = response.message;
+
+    result.forEach(breed => {
+      const img = document.createElement("img");
+      img.src = breed;
+      img.alt = "Cute doggo";
+      doggos.appendChild(img);
+    })
+
+
+  }
+
+
+  xhr.open("GET", url);
+  xhr.send();
+}
